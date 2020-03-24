08542eac5843b951d0fff543b25d0a53206df5e9
akanehl@ucsc.edu <akanehl@ucsc.edu>
2020-03-24 16:45:14 -0700

    finished proj

---
+++ prefabs/Rocket.js
@@ -0,0 +1,39 @@
+// Rocket prefab
+class Rocket extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture, frame) {
+      super(scene, x, y, texture, frame);
+      // add object to existing scene
+      scene.add.existing(this);
+      this.isFiring = false;
+      this.sfxRocket = scene.sound.add('sfx_rocket'); // add rocket sfx
+    }
+
+    update() {
+        if(!this.isFiring) {
+            if(keyLEFT.isDown && this.x >=47) {
+                this.x -=2;
+            }
+            else if(keyRIGHT.isDown && this.x <=598) {
+                this.x +=2;
+            }
+        }
+
+        if(Phaser.Input.Keyboard.JustDown(keyF)) {
+            this.isFiring = true;
+            this.sfxRocket.play();
+        }
+
+        if(this.isFiring &&this.y >= 108) {
+            this.y -=2;
+        }
+
+        if(this.y<= 108) {
+            this.reset();
+        }
+    }
+
+    reset() {
+        this.isFiring = false;
+        this.y = 431;
+    }
+  }
